2ba8064834967b52ff2aaf6ba1347cc455dcb552
tobias.theil <tobias.theil@AMS-GYM-EDV1-05.gym.ams.local>
2019-02-11 16:15:02 +0100

    Bubblesort geschaft

---
+++ HalloWeltProjekt/src/sort/Bubblesort.java
@@ -0,0 +1,29 @@
+package sort;
+
+public class Bubblesort {
+
+	public static void main(String[] args) {
+
+		int[] zahlen = { 3, 6, 1, 9, 2, 5 };
+		//
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
+
+		for (int n = zahlen.length; n > 1; n--) {
+			for (int i = 0; i < n - 1; i++) {
+				if (zahlen[i] > zahlen[i + 1]) {
+					int help = zahlen[i];
+					zahlen[i] = zahlen[i + 1];
+					zahlen[i + 1] = help;
+
+				}
+			}
+
+		}
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+
+		}
+	}
+}
