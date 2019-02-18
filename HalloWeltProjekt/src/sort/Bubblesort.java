bcae71de38144ea73ea747f526b6b53e7c73f65e
tobias.theil <tobias.theil@AMS-GYM-EDV1-05.gym.ams.local>
2019-02-18 15:01:00 +0100

    Bubblesort fertig

--- HalloWeltProjekt/src/sort/Bubblesort.java
+++ HalloWeltProjekt/src/sort/Bubblesort.java
@@ -10,6 +10,10 @@ public class Bubblesort {
 			System.out.println(zahlen[i]);
 		}
 
+			System.out.println("-----------");
+		
+		
+				
 		for (int n = zahlen.length; n > 1; n--) {
 			for (int i = 0; i < n - 1; i++) {
 				if (zahlen[i] > zahlen[i + 1]) {
