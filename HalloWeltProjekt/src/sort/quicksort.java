3491e5605d9ca143345cba87735777887700f2a6
Tobias.theil <Tobias.theil@AMS-GYM-EDV1-05.gym.ams.local>
2019-03-04 16:16:59 +0100

    Quicksort begonnen

--- HalloWeltProjekt/src/sort/quicksort.java
+++ HalloWeltProjekt/src/sort/quicksort.java
@@ -2,14 +2,62 @@ package sort;
 
 public class quicksort {
 
+	static int[] zahlen = { 9, 2, 5, 1, 7, 10, 6, 4, 3 };
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int links = 0;
+		int rechts = zahlen.length - 1;
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
+		//
+		quicksort(links, rechts);
+		//
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
 	}
 
 	public static void quicksort(int links, int rechts) {
-		if (links < rechts) {
 
+		if (links < rechts) {
+			int teiler = teile(links, rechts);
+			quicksort(links, teiler-1);
+			quicksort(teiler+1, rechts);
 		}
+		
+		
+	}
+	
+
+	public static int teile(int links, int rechts) {
+		int i = links;
+		int j = rechts - 1;
+		int pivot = zahlen[rechts];
+
+		do {
+			while (i < rechts - 1 && zahlen[i] < pivot) {
+				i = i + i;
+			}
+
+			while (j > links && zahlen[i] >= pivot) {
+				j = j - j;
+			}
+			if (i < j) {
+				int help = zahlen[i];
+				zahlen[i] = zahlen[j];
+				zahlen[j] = help;
+			}
+
+		} while (i < j);
+
+		if (zahlen[i] > pivot) {
+			int help = zahlen[i];
+			zahlen[i] = zahlen[rechts];
+			zahlen[rechts] = help;
+		}
+		return i;
 
 	}
 }
